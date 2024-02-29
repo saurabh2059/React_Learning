d90094944fe4bcd73f2e580916f2b7029fb599eb
saurabh2059 <aryalsaurav141@gmail.com>
2024-02-29 18:39:26 +0545

    changed

--- 08currency/src/App.jsx
+++ 08currency/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import InputBox from './components/InputBox'
+import {InputBox} from './components'
 import useCurrencyInfo from './hooks/useCurrencyInfo'
 
 
@@ -14,12 +14,14 @@ function App() {
 
   const options = Object.keys(currencyInfo)
 
-  const swap = () => {
-    setFrom(to)
-    setTo(from)
-    setConvertedAmount(amount)
-    setAmount(convertedAmount)
-  }
+//   const swap = () => {
+//     setFrom(to)
+//     setTo(from)
+//     setConvertedAmount(amount)
+//     setAmount(convertedAmount)
+//   }
+
+
   
   const convert = () => {
     setConvertedAmount(amount * currencyInfo[to])
@@ -46,12 +48,12 @@ function App() {
                             label="From"
                             amount={amount}
                             currencyOptions={options}
-                            onCurrencyChange={(currency) => setAmount(amount)}
+                            onCurrencyChange={(currency) => setFrom(currency)}
                             selectCurrency={from}
                             onAmountChange={(amount) => setAmount(amount)}
                         />
                     </div>
-                    <div className="relative w-full h-0.5">
+                    {/* <div className="relative w-full h-0.5">
                         <button
                             type="button"
                             className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
@@ -59,11 +61,11 @@ function App() {
                         >
                             swap
                         </button>
-                    </div>
+                    </div> */}
                     <div className="w-full mt-1 mb-4">
                         <InputBox
                             label="To"
-                            amount={convertedAmount.toFixed(2)}
+                            amount={convertedAmount}
                             currencyOptions={options}
                             onCurrencyChange={(currency) => setTo(currency)}
                             selectCurrency={to}
@@ -73,7 +75,7 @@ function App() {
                     <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
                         Convert {from.toUpperCase()} to {to.toUpperCase()}
                     </button>
-                </form> 
+                </form>
             </div>
         </div>
     </div>
--- 08currency/src/hooks/useCurrencyInfo.js
+++ 08currency/src/hooks/useCurrencyInfo.js
@@ -1,13 +1,16 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react"
 
-function useCurrenceInfo(currency){
-   const [data,setData] = useState({})
 
-    useEffect(()=>{
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`).then((res) => res.json()).then((res)=>setData(res[currency]))
+function useCurrencyInfo(currency){
+    const [data, setData] = useState({})
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        .then((res) => res.json())
+        .then((res) => setData(res[currency]))
         console.log(data);
-    },[currency])
-    return data;
+    }, [currency])
+    console.log(data);
+    return data
 }
-export default useCurrenceInfo;
 
+export default useCurrencyInfo;
